53bd76a6f8775f49ed22e63d121fa06933e86642
vrushankpatel <patelvd99@gmail.com>
2025-06-11 14:24:08 -0400

    issue#1 - start new match functionality

--- app/src/main/java/com/vrushank/crickstars/data/room/TeamDao.kt
+++ app/src/main/java/com/vrushank/crickstars/data/room/TeamDao.kt
@@ -1,17 +1,43 @@
 package com.vrushank.crickstars.data.room
 
 import androidx.room.Dao
+import androidx.room.Embedded
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Relation
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface TeamDao {
     @Insert
     suspend fun insertTeam(team: Team):Long
     @Insert
     suspend fun insertPlayer(player: Player)
-    @Query("SELECT * FROM TEAM")
+
+    @Query("SELECT * FROM Team")
     suspend fun getTeams():List<Team>
+
     @Query("SELECT * FROM Player WHERE teamOwnerId = :teamId")
     suspend fun getPlayersForTeam(teamId:Int):List<Player>
 
+    @Transaction
+    @Query("SELECT * FROM Team")
+    fun getTeamsWithPlayers(): Flow<List<TeamWithPlayers>>
+
+    @Query("DELETE FROM Player")
+    suspend fun deleteAllPlayers()
+
+    @Query("DELETE FROM Team")
+    suspend fun deleteAllTeams()
+
 }
+
+data class TeamWithPlayers(
+    @Embedded val team: Team,
+    @Relation(
+        parentColumn = "teamId",
+        entityColumn = "teamOwnerId"
+    )
+    val players: List<Player>
+)
--- app/src/main/java/com/vrushank/crickstars/presentation/SelectTeamPlayers.kt
+++ app/src/main/java/com/vrushank/crickstars/presentation/SelectTeamPlayers.kt
@@ -1,6 +1,7 @@
 package com.vrushank.crickstars.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,14 +10,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,16 +31,29 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.vrushank.crickstars.R
+import com.vrushank.crickstars.data.room.TeamWithPlayers
+import kotlinx.coroutines.flow.StateFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SelectTeamPlayers(teamInit: () -> String, teamName: (String) -> Unit, addTeamName: () -> Unit) {
+fun SelectTeamPlayers(
+    teamInit: () -> String,
+    teamName: (String) -> Unit,
+    addTeamName: () -> Unit,
+    teamsList: StateFlow<List<TeamWithPlayers>>,
+    delete: () -> Unit,
+    playerInit: () -> String,
+    playerName : (String) -> Unit,
+    addPlayerName:() -> Unit
+) {
 //    Column(
 //        horizontalAlignment = Alignment.Start,
 //        verticalArrangement = Arrangement.Center
 //    ) {
 //        TextField()
 //    }
+
+    val teams by teamsList.collectAsState()
     Box {
         Image(
             painter = painterResource(R.drawable.team_select_new),
@@ -53,12 +72,18 @@ fun SelectTeamPlayers(teamInit: () -> String, teamName: (String) -> Unit, addTea
             TextField(
                 value = teamInit(),
                 onValueChange = { teamName(it) },
-                label = { Text("Team Name", color = Color(0xFF2E7D32), fontWeight = FontWeight.Bold) },
+                label = {
+                    Text(
+                        "Team Name",
+                        color = Color(0xFF2E7D32),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
                 colors = TextFieldDefaults.textFieldColors(
                     containerColor = Color.Transparent,
                     focusedIndicatorColor = Color(0xFF2E7D32),
                     focusedTextColor = Color(0xFF2E7D32),
-                    unfocusedTextColor = Color.Black,
+                    unfocusedTextColor = Color(0xFF4CAF50),
                     cursorColor = Color(0xFF2E7D32),
                     unfocusedIndicatorColor = Color.Black
 
@@ -69,9 +94,11 @@ fun SelectTeamPlayers(teamInit: () -> String, teamName: (String) -> Unit, addTea
             Button(
                 onClick = { addTeamName() },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2E7D32)),
-                modifier = Modifier.fillMaxWidth().padding(top = 12.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
             ) {
-                Text("Add Team",color = Color.White)
+                Text("Add Team", color = Color.White)
             }
 
 
@@ -79,27 +106,61 @@ fun SelectTeamPlayers(teamInit: () -> String, teamName: (String) -> Unit, addTea
             Spacer(modifier = Modifier.height(16.dp))
 
             TextField(
-                value = "",
-                onValueChange = { },
-                label = { Text("Player Name" ,color = Color(0xFF2E7D32), fontWeight = FontWeight.Bold) },
+                value =playerInit(),
+                onValueChange = { playerName(it)},
+                label = {
+                    Text(
+                        "Player Name",
+                        color = Color(0xFF2E7D32),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
                 colors = TextFieldDefaults.textFieldColors(
                     containerColor = Color.Transparent,
                     focusedIndicatorColor = Color(0xFF2E7D32),
                     focusedTextColor = Color(0xFF2E7D32),
-                    unfocusedTextColor = Color.Black,
+                    unfocusedTextColor = Color(0xFF4CAF50),
                     cursorColor = Color(0xFF2E7D32),
                     unfocusedIndicatorColor = Color.Black
-                    )
+                )
             )
 
             Button(
-                onClick = { },
+                onClick = { addPlayerName()},
                 //enabled = viewModel.lastTeamId != null,
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2E7D32)),
-                modifier = Modifier.fillMaxWidth().padding(top = 12.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
             ) {
-                Text("Add Player to Team",color = Color.White)
+                Text("Add Player to Team", color = Color.White)
             }
+
+            Button(
+                onClick = { delete() },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2E7D32)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+            ) {
+                Text("Remove all",color = Color.White)
+            }
+
+            LazyColumn(modifier = Modifier.padding(16.dp).background(Color(0xFF2E7D32))) {
+                items(teams) { teamWithPlayers ->
+                    Text(
+                        "Team: ${teamWithPlayers.team.name}",
+                        color = Color.White,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                    teamWithPlayers.players.forEach { player ->
+                        Text(" - ${player.name}", color = Color.White,modifier = Modifier.padding(start = 16.dp))
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+            }
+
         }
     }
 }
